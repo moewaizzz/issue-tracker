dadd4ec1d18e5868edd5f453216c68e5e999bfa6
moewaizzz <moewai3127@gmail.com>
2024-02-07 02:33:14 +0630

    improve the loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Button, Callout, Text, TextField } from "@radix-ui/themes";
-import dynamic from "next/dynamic";
 import { useForm, Controller, FieldValues } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
@@ -12,10 +11,7 @@ import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 import { Issue } from "@prisma/client";
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
+import SimpleMDE from "react-simplemde-editor";
 
 type IssueFormData = z.infer<typeof IssueSchema>;
 

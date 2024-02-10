2b1ff1156777921c6e8647dd9e409d36c5930abc
moewaizzz <moewai3127@gmail.com>
2024-02-10 20:01:30 +0630

    build the issueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
-import Image from "next/image";
-import { SessionProvider } from "next-auth/react";
-import { Session } from "next-auth";
-import { Button } from "@radix-ui/themes";
-import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inprogress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
   return (
     <>
+      <IssueSummary open={open} inProgress={inprogress} closed={closed} />
       <LatestIssues />
     </>
   );
